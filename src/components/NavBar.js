adb7bed316f3176c2cf12201dd3849e10185c2aa
Esteban Lepe <esteban.lepe1c@gmail.com>
2022-06-11 03:06:53 -0400

    Preparando build

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ export const NavBar = () => {
     return (
         <nav className="navbar navbar-expand-sm fixed-top navbar-dark royal-nav">
             <div className="container-fluid">
-                <img src={Logo} alt="Imagen de logo" width="5%" /> &nbsp;
+                <img src={require("../img/Logo.png")} alt="Imagen de logo" width="5%" /> &nbsp;
                 <Link className="navbar-brand" to="/">Gym-App</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
