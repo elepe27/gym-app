06ba14096f73784a7094baaabdb97d45a2895ca5
Esteban Lepe <esteban.lepe1c@gmail.com>
2022-06-11 03:15:39 -0400

    Preparando build 2

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link, NavLink } from "react-router-dom";
-import Logo from "../img/Logo.png";
+
 
 export const NavBar = () => {
 
     return (
         <nav className="navbar navbar-expand-sm fixed-top navbar-dark royal-nav">
             <div className="container-fluid">
-                <img src={require("../img/Logo.png")} alt="Imagen de logo" width="5%" /> &nbsp;
+                <img src={require("../Logo.png")}    alt="Imagen de logo" width="5%" /> &nbsp;
                 <Link className="navbar-brand" to="/">Gym-App</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
